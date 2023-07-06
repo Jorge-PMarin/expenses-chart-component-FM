07e0b4a98015f67ce7e49ea02235c480de69de6a
jorge luis puerta marin <jorgeluis.sk8@hotmail.com>
2023-07-06 11:40:20 -0500

    component finished

---
+++ README.md
@@ -0,0 +1,27 @@
+### Features
+
+Users are able to:
+
+- View the bar chart and hover over the individual bars to see the correct amounts for each day
+- See the current day’s bar highlighted in a different colour to the other bars
+- View the optimal layout for the content depending on their device’s screen size
+- See hover states for all interactive elements on the page
+- Uses the JSON data file provided to dynamically size the bars on the chart
+
+### Links
+
+- Solution URL: https://github.com/Jorge-PMarin/expenses-chart-component-FM
+- Live Site URL: https://jorge-pmarin.github.io/expenses-chart-component-FM/
+
+### Built with
+
+- Semantic HTML5 markup
+- SASS/SCSS
+- Flexbox
+- Mobile-first workflow
+
+### Screenshot
+
+![desktop-design](./my-design/my-design-desktop.png)
+![active-states](./my-design/my-design-active-states.png)
+![mobile-design](./my-design/my-design-mobile.png)
--- src/script.js
+++ src/script.js
@@ -1,5 +1,5 @@
-const expenseLabels = document.querySelectorAll('.expenses__item__label');
-const expenseBars = document.querySelectorAll('.expenses__item__bar');
+const expenseLabels = document.querySelectorAll(".expenses__item__label");
+const expenseBars = document.querySelectorAll(".expenses__item__bar");
 
 function calculate(data) {
   //get amounts
@@ -9,23 +9,50 @@ function calculate(data) {
   const highest = Math.max(...amounts);
 
   //calculate bar size in relation to the highest amount(100%)
-  const barSizes = amounts.map(amount => Math.round((amount * 100) / highest));
-  console.log(barSizes);
+  const barSizes = amounts.map((amount) =>
+    Math.round((amount * 100) / highest)
+  );
 
   //set bar size for each bar
   for (let i = 0; i < amounts.length; i++) {
-    expenseBars[i].style.height = barSizes[i].toString() + '%'
+    expenseBars[i].style.height = barSizes[i].toString() + "%";
   }
 
   //set amount for each label
   for (let i = 0; i < expenseLabels.length; i++) {
-    expenseLabels[i].textContent = amounts[i].toString()
-    expenseLabels[i].style.bottom = (barSizes[i]).toString() + '%'
+    expenseLabels[i].textContent = "$" + amounts[i].toString();
+    expenseLabels[i].style.bottom = (barSizes[i] + 6).toString() + "%";
   }
-
 }
 
-fetch('../data.json')
+function highlightCurrentDay() {
+  const date = new Date();
+  const today = date.getDay();
+  const bar = expenseBars[today - 1];
+  bar.classList.add("expenses__item__bar--today");
+}
+
+//show the amount of a specific day when hovered
+function handleHover() {
+  expenseBars.forEach((bar) => {
+    //display amount label when hovered
+    bar.addEventListener("mouseover", () => {
+      const amountLabel = bar.parentElement.children[0];
+      amountLabel.classList.add("expenses__item__label--active");
+    });
+
+    //hide amount label on mouseout
+    bar.addEventListener("mouseout", () => {
+      const amountLabel = bar.parentElement.children[0];
+      amountLabel.classList.remove("expenses__item__label--active");
+    });
+  });
+}
+
+highlightCurrentDay();
+handleHover();
+
+fetch("../data.json")
   .then((res) => res.json())
   .then((data) => {
     jsonData = data;
